771a114ab3ffd122134d102eb772a419da8f6b67
Jarosław Wójcik <j.wojcik@interia.eu>
2017-11-24 15:18:31 +0100

    Change scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,14 @@ var a = prompt ("Enter value a");
 var b = prompt ("Enter value b");
 var value = (a * a) - (2 * a * b) + (b * b);
 console.log ("The score is : " +value);
-
-if (value == 0 ) {
-    console.log ("The score is equal to ZERO")
-} else if (value > 0) {
+ 
+//Checking whether the result is positive or negative
+ if (value > 0) {
     console.log ("The score is positive")
-} else {
+} else if (value < 0) {
     console.log ("The score is negative")
 }
+//Checking whether the result is equal to 0
+if (value == 0 ) {
+    console.log ("The score is equal to ZERO")
+} 
